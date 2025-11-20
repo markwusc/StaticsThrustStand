99fa2f9c171effe38a6ed897eecb282a61433127
markwusc <markaw@usc.edu>
2025-11-19 16:50:30 -0800

    Add files via upload

--- StaticsThrustStand.m
+++ StaticsThrustStand.m
@@ -1,11 +1,18 @@
 %Simulation for statics thrust stand
 
+%Adjust filePath to your machine
+filePath = "C:\Users\Mark\Downloads\Estes_C11.csv";
 
-M = readmatrix("C:\Users\Mark\Downloads\Estes_C11.csv");
+%Read predicted values
+projectedThrust = readmatrix(filePath);
+
+%Read real data
+% loadThrust = readmatrix("");
+% loadCompression = readmatrix("");
 
 % Extract relevant columns for analysis, assuming first column is time and second is thrust
-time = M(:, 1);
-thrust = M(:, 2);
+time = projectedThrust(:, 1);
+thrust = projectedThrust(:, 2);
 
 % Plot the thrust over time
 % figure;
@@ -22,7 +29,7 @@ averageThrust = mean(thrust);
 MotorForce = thrust;
 BeamCompression = sqrt(2) * thrust / 2;
 
-%Graph the load cell results compared to time
+%Graph the predicted results compared to time
 figure;
 plot(time, MotorForce, time, BeamCompression);
 xlabel('Time (s)');
@@ -31,3 +38,11 @@ title('Load Cell Force Profiles vs Time');
 legend('Motor Thrust (N)', 'Beam compression force (N)');
 grid on;
 
+%Graph the load cell results compared to time
+figure;
+plot(time, MotorForce, time, BeamCompression);
+xlabel('Time (s)');
+ylabel('Load Cell Force (N)');
+title('Load Cell Force Profiles vs Time');
+legend('Motor Thrust (N)', 'Beam compression force (N)');
+grid on;
