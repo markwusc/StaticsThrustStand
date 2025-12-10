48c5b2528964f78919291976f5ea47b9025ff3ef
Gus Fisher <gafisher@usc.edu>
2025-12-09 19:10:46 -0800

    stuff

--- Arduino/hx711_loadcell_calibration/hx711_loadcell_calibration.ino
+++ Arduino/hx711_loadcell_calibration/hx711_loadcell_calibration.ino
@@ -6,9 +6,14 @@ const int LOADCELL_SCK_PIN = 3;
 
 HX711 loadcell;
 
+String readLine() {
+  while (!Serial.available()) {}      // wait for input
+  return Serial.readStringUntil('\n');
+}
+
 void setup() {
-  Serial.begin(115200)
-  loadcell.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN)
+  Serial.begin(115200);
+  loadcell.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
 
   if (loadcell.is_ready()) {
     // Set load cell scale to default
@@ -22,7 +27,7 @@ void setup() {
 
     // Measure a known weight
     Serial.println("PLACE KNOWN WEIGHT ON LOADCELL, MEASURING IN 30 SECONDS");
-    delay(30000);
+    delay(5000);
     long reading = loadcell.get_units(10);
     Serial.print("RESULT (ADC): ");
     Serial.println(reading);
@@ -35,10 +40,10 @@ void setup() {
     Serial.print("CALIBRATION FACTOR: ");
     Serial.println(calibration_factor);
 
-    delay(5000)
+    delay(5000);
   }
   else {
-    Serial.println("HX711 MODULE NOT FOUND")
+    Serial.println("HX711 MODULE NOT FOUND");
   }
 }
 
@@ -46,9 +51,10 @@ void loop() {
   // Measure data and show on terminal screen (WILL NOT STORE DATA, USE PYTHON SCRIPT FOR THAT)
   if (loadcell.is_ready()) {
     unsigned long t = micros(); // TIMESTAMP IN MICROSECONDS
-    long rawData = loadcell.read()
+    long rawData = loadcell.read();
     Serial.print(t);
     Serial.print(",");
-    Serial.print(rawData)
+    Serial.println(rawData);
+    
   }
 }
--- hx711LoadCellReader.py
+++ hx711LoadCellReader.py
@@ -1,9 +1,10 @@
+from time import sleep
 import serial
 import csv
 
-PORT = "/dev/tty.usbmodemXXXX" # unknown until arduino plugged in, run ls /dev/tty.*
+PORT = "/dev/tty.usbmodem1401" # unknown until arduino plugged in, run ls /dev/tty.*
 BAUD = 115200 # based on Serial.begin(######) command in .ino file
-CSV_FILE = "loadCellData.csv" # name of csv file
+CSV_FILE = "/Users/gusfisher/Desktop/loadCellData.csv" # name of csv file
 
 # Open the serial port, if no new data for 1 second throw error
 ser = serial.Serial(PORT, BAUD, timeout=1)
@@ -12,14 +13,16 @@ with open(CSV_FILE, "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(["time_microseconds", "raw_value"])
 
-    print("LOGGING. PRESS CMD+C TO STOP")
+    print("LOGGING. PRESS CTRL+C TO STOP")
+    sleep(1)
     while True:
         try:
             # Read line, convert to string
-            line = ser.readLine().decode()
+            line = ser.readline().decode()
 
             # Split string at comma
             t, raw = line.split(",")
+            print(t + ',' + raw)
             writer.writerow([t, raw])
 
         # Break if CMD+C
