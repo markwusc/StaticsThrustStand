b560d196ebfcf44945ade276d68ee5ad7a48fbdb
Gus Fisher <gafisher@usc.edu>
2025-12-12 21:30:34 -0800

    adding double loadcell...sketchy atm

--- Arduino/stream_loadcell/stream_loadcell.ino
+++ Arduino/stream_loadcell/stream_loadcell.ino
@@ -1,15 +1,19 @@
 #include <HX711.h>
 #include <EEPROM.h>
 
-const int LOADCELL_DOUT = 2;
-const int LOADCELL_SCK  = 3;
+const int LOADCELL1_DOUT = 2;
+const int LOADCELL1_SCK  = 3;
+const int LOADCELL2_DOUT = 4;
+const int LOADCELL2_SCK  = 5;
 
-HX711 scale;
+HX711 loadcell1;
+HX711 loadcell2;
 float cal_factor = 1.0f;
 
 void setup() {
   Serial.begin(115200);
-  scale.begin(LOADCELL_DOUT, LOADCELL_SCK);
+  loadcell1.begin(LOADCELL1_DOUT, LOADCELL1_SCK);
+  loadcell2.begin(LOADCELL2_DOUT, LOADCELL2_SCK);
 
   EEPROM.get(0, cal_factor);
   if (isnan(cal_factor) || cal_factor < 0.001) {
@@ -19,20 +23,28 @@ void setup() {
   Serial.print("Loaded calibration factor: ");
   Serial.println(cal_factor, 6);
 
-  scale.set_scale();
-  scale.tare();
+  loadcell1.set_scale();
+  loadcell1.tare();
+  loadcell2.set_scale();
+  loadcell2.tare();
 }
 
 void loop() {
-  if (!scale.is_ready()) return;
+  if (!loadcell1.is_ready() && loadcell2.is_ready()) return;
 
   unsigned long t = micros();
-  long raw = scale.read();
-  float grams = raw / cal_factor;
+  long raw1 = loadcell1.read();
+  float grams1 = raw1 / cal_factor;
+  long raw2 = loadcell2.read();
+  float grams2 = raw2 / cal_factor;
 
   Serial.print(t);
   Serial.print(",");
-  Serial.print(raw);
+  Serial.print(raw1);
   Serial.print(",");
-  Serial.println(grams, 4);
+  Serial.print(grams1, 4);
+  Serial.print(",");
+  Serial.print(raw2);
+  Serial.print(",");
+  Serial.println(grams2, 4);
 }
